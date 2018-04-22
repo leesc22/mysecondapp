942440112321cc2b3e4d654e178807f0a2cc9eeb
leesc22 <leesiewching22@gmail.com>
2018-04-22 20:50:44 +0800

    Create a second activity

--- app/src/main/java/com/example/mysecondapp/MainActivity.kt
+++ app/src/main/java/com/example/mysecondapp/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.mysecondapp
 
+import android.content.Intent
 import kotlinx.android.synthetic.main.activity_main.textView
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
@@ -28,5 +29,22 @@ class MainActivity : AppCompatActivity() {
 
         // Display the new value in the text view
         textView.text = count.toString();
+    }
+
+    fun randomMe (view: View) {
+        // Create an Intent to start the second activity
+        val randomIntent = Intent(this, SecondActivity::class.java)
+
+        // Get the current value of the text view
+        val countString = textView.text.toString()
+
+        // Convert the count to an int
+        val count = Integer.parseInt(countString)
+
+        // Add the count to the extras for the Intent
+        randomIntent.putExtra(SecondActivity.TOTAL_COUNT, count)
+
+        // Start the new activity
+        startActivity(randomIntent)
     }
 }
